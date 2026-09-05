2681b291282b591fdebf920bd9a1b8bc4c8889a8
Shubham Chandrakar <shubhamchadrakar16@gamil.com>
2026-09-06 00:41:44 +0530

    Implement JWT authentication filter and CORS handling

--- Salessavvy_app/src/main/java/com/projects/salessavvy_app/repositories/JWTTokenRepository.java
+++ Salessavvy_app/src/main/java/com/projects/salessavvy_app/repositories/JWTTokenRepository.java
@@ -1,6 +1,8 @@
 package com.projects.salessavvy_app.repositories;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,8 @@ public interface JWTTokenRepository extends JpaRepository<JWTToken,Integer> {
 
 	@Query("SELECT t FROM JWTToken t WHERE t.user.userId = :userId")
 	JWTToken findByUserId(@Param("userId") int userId);
+	
+	Optional<JWTToken> findByToken(String token);
 	
 }
 
